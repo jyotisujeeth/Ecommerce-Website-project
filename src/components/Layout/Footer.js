2be52f78b8f7c87eaf8ffa84729ec1828e4030f6
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-30 05:05:08 +0530

    eduted layout infor

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,40 +1,19 @@
-import React from "react";
-import "./Footer.module.css";
+import { Fragment } from "react";
+import { BsYoutube, BsSpotify, BsFacebook, BsInstagram } from "react-icons/bs";
+
 const Footer = () => {
   return (
-    <footer>
-      <h4>The Generics</h4>
-      <div>
-        <ul>
-          <li>
-            <a href="https://www.youtube.com/">
-              <img
-                src="https://prasadyash2411.github.io/ecom-website/img/6260efc8fc9a9002669d2f4ad9956cc0.jpg"
-                alt="YouTube"
-              />
-            </a>
-            
-          </li>
-          <li>
-            <a href="https://open.spotify.com//">
-              <img
-                src="https://prasadyash2411.github.io/ecom-website/img/Spotify%20Logo.png"
-                alt="Spotify"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/">
-              <img
-                src="https://prasadyash2411.github.io/ecom-website/img/Facebook%20Logo.png"
-                alt="Facebook"
-              />
-            </a>
-          </li>
-        </ul>
+    <Fragment>
+      <div className="p-1 bg-info text-white d-flex align-items-center ">
+        <p style={{ fontSize: "3em", fontFamily: "bold", marginLeft: "25%" }}>
+          The Generics
+        </p>
+        <BsYoutube style={{ marginLeft: "40%" }} size={36} />
+        <BsSpotify style={{ marginLeft: "2%" }} size={36} />
+        <BsFacebook style={{ marginLeft: "2%" }} size={36} />
+        <BsInstagram style={{ marginLeft: "2%" }} size={36} />
       </div>
-    </footer>
+    </Fragment>
   );
 };
-
 export default Footer;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,145 @@
-const Header = () => {
+import { Fragment, useState, useContext, useEffect } from "react";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import Cart from "../Cart/Cart";
+import { BsCart3 } from "react-icons/bs";
+import CartContext from "../Cart/CartContext";
+import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import AuthContext from "../Store/AuthContext";
+
+const Header = (props) => {
+  const [showCart, setShowCart] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const authCtx = useContext(AuthContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  // Whenever the user who has not logged In clicks on the Products tab should be redirected to Login Page automatically.
+
+  const history = useNavigate();
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    alert("Do you want Logout");
+    authCtx.Logout();
+    history("/login");
+  };
+  const openCart = () => {
+    setShowCart(true);
+
+    // fetch(
+    //   `https://ecommerse1-c3862-default-rtdb.firebaseio.com/ProductData/${authCtx.email}.json`
+    // )
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   console.log(data);
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+  };
+
+  useEffect(() => {
+    console.log("successfully run");
+    openCart();
+  }, []);
+
+  const closeCart = () => {
+    setShowCart(false);
+  };
+
+  const location = useLocation();
   return (
-    <div>
-      <div className="mt-5">
-        <div style={{ color: "black", textAlign: "center" }}>
-          <h1>The Generics</h1>
-          <h2>Ecommerce Website</h2>
-        </div>
-      </div>
-    </div>
+    <Fragment>
+      <Navbar bg="dark" expand="sm" variant="dark">
+        <Container className="justify-content-center">
+          <Nav>
+            <Nav.Item style={{ marginRight: "40px" }}>
+              <NavLink to="/home" style={{ color: "white" }}>
+                HOME
+              </NavLink>
+            </Nav.Item>
+
+            {isLoggedIn && (
+              <div style={{ marginRight: "40px" }}>
+                <Link to="/store" style={{ color: "green" }}>
+                  STORE
+                </Link>
+              </div>
+            )}
+
+            <Nav.Item style={{ marginRight: "40px" }}>
+              <NavLink
+                to="/about"
+                style={{ color: "white" }}
+                onClick={() => props.handleShow(false)}
+              >
+                ABOUT
+              </NavLink>
+            </Nav.Item>
+
+            <Nav.Item style={{ marginRight: "40px" }}>
+              <NavLink to="/contactUs" style={{ color: "white" }}>
+                ContactUs
+              </NavLink>
+            </Nav.Item>
+
+            <Nav.Item style={{ marginRight: "40px" }}>
+              <NavLink
+                to="/auth"
+                style={{ color: "yellow" }}
+                render={(props) =>
+                  isLoggedIn ? (
+                    <NavLink {...props} />
+                  ) : (
+                    <NavLink {...props} disabled />
+                  )
+                }
+              >
+                Login
+              </NavLink>
+            </Nav.Item>
+
+            {isLoggedIn && (
+              <div style={{ marginRight: "40px" }}>
+                <Link
+                  to="/auth"
+                  style={{ color: "red" }}
+                  onClick={logoutHandler}
+                >
+                  Logout
+                </Link>
+              </div>
+            )}
+          </Nav>
+        </Container>
+        {location.pathname !== "/" &&
+          location.pathname !== "/about" &&
+          location.pathname !== "/contact_us" &&
+          location.pathname !== "/auth" && (
+            <Button
+              variant="outline-primary"
+              style={{
+                marginRight: "15px",
+                backgroundColor: "transparent",
+                borderColor: "#007bff",
+                color: "white",
+              }}
+              onClick={openCart}
+            >
+              Cart <BsCart3 />
+              {numberOfCartItems}
+            </Button>
+          )}
+      </Navbar>
+
+      {showCart && <Cart onClose={closeCart} />}
+    </Fragment>
   );
 };
 export default Header;
